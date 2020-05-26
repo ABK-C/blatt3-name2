60e79d127503ff76816aad9c1da95765f55e934a
Kn0wledg3 <lott.dominic@web.de>
2020-05-26 11:02:38 +0000

    Blatt3

--- likelihood.cc
+++ likelihood.cc
@@ -1,20 +1,66 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 
 
 double poisson(double mu, int k) {
-    return 0;
+    
+    double mu_k = pow(mu, k);
+    double e_mu = exp(-mu);
+    double k_fac = tgamma(k+1);
+
+    return (mu_k*e_mu/k_fac);
 }
 
-int main() {
-    using namespace std;
+int main(){
+  
+  using namespace std;
+  
+  int n_i;
+  double mu = 3.11538;
+  double li = 1;
+  double l = 1;
+  
+  ifstream fin("datensumme.txt");
+  ofstream like("likelihood.txt");
+  ofstream like2("likelihood2.txt");
+  
+  vector<int> daten;
 
+  for(int i = 0; i < 234; i++){
+    
+    fin >> n_i;
+    daten.push_back(n_i);
+  
+  }
 
-    ifstream fin("datensumme.txt");
-    int n_i;
-    for(int i = 0 ; i < 234 ; ++i) {
-        fin >> n_i;
-    }
-    fin.close();
+  for(int k : daten){
+    
+    double li = poisson(mu, k);
+    
+    l *= li;
+  }
+
+  cout << l << endl;
+
+  for(double j = 0; j < 6;){
+    
+    double h = 1;
+    
+      for(int n : daten){
+      
+        double hi = poisson(j, n);
+      
+        h *= hi;
+      }
+
+    like << j <<" "<< h << endl;
+    like2 << h << endl;
+    j += 0.01;
+  }
+
+fin.close();
+like.close();
+like2.close();
 }
--- poisson.cc
+++ poisson.cc
@@ -1,20 +1,55 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
+#define N 234
+/*#define double mu 3.11538 ???? */
 
 
 double poisson(double mu, int k) {
-    return 0;
+    
+    double mu_k = pow(mu, k);
+    double e_mu = exp(-mu);
+    double k_fac = tgamma(k+1);
+
+    return (mu_k*e_mu/k_fac);
 }
 
 int main() {
+    
     using namespace std;
 
-
-    ifstream fin("datensumme.txt");
     int n_i;
+    int j = 0;
+    double mu = 3.11538;
+    double p;
+    
+    ifstream fin("datensumme.txt");
+    ofstream hist("hist.txt");
+    ofstream histpoi("histpoi.txt");
+    
+    
+    
+    vector<int> zaehler(11);
+    
     for(int i = 0 ; i < 234 ; ++i) {
-        fin >> n_i;
+      fin >> n_i;
+      zaehler[n_i] +=1;
+        
     }
+    
+    for(vector<int>::const_iterator h = zaehler.begin(); h != zaehler.end(); ++h)
+    {
+      
+      p = poisson(mu,j);
+
+      hist << j << " " << *h << endl;
+      histpoi << j << " "<< *h << " " << N*p << endl;
+      
+      ++j;
+    }
+
     fin.close();
+    hist.close();
+    histpoi.close();
 }
